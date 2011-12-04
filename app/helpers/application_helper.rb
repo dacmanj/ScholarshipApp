85e45e3639de43ffc8bf0f5f7c8b1ab1bb48a443
David Manuel <david@dcmanjr.com>
2011-12-03 23:51:57 -0500

    layout initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def logo
+    image_tag("logo.jpg", :alt => "PFLAG National", :class => "round")
+  end
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "PFLAG National Scholarship Application"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
